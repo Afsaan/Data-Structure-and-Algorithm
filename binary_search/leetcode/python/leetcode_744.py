5429835967971057a77d1fec3cd30819c5fee1c2
Afsan <khanafsaan11@gmail.com>
2022-12-01 17:02:08 +0530

    code fix

--- binary_search/leetcode/python/leetcode_744.py
+++ binary_search/leetcode/python/leetcode_744.py
@@ -2,19 +2,23 @@ def ceiling(array, target):
     start_point = 0
     end_point = len(array)-1
 
-    if target > array[end_point]:
-        return array[start_point]
-    while(start_point<=end_point):
+    while start_point <= end_point:
         middle_point = (start_point + end_point)//2
 
-        if array[middle_point] > target:
-            end_point = middle_point-1
-        elif array[middle_point] < target:
-            start_point = middle_point+1
-    return array[start_point%len(array)]
+        if target > array[middle_point]: 
+            start_point = middle_point + 1
+        else:
+            end_point = middle_point - 1
+            
 
-array = ["x","x","y","y"]
-target = "z"
+    if start_point == len(array):
+        return array[0]
+    else:
+        return array[start_point]
+        
+
+array = ["c","f","j","y"]
+target = "d"
 
 ans = ceiling(array, target)
 print(ans)
